4188654415c513384c461fea0218dd6fdabfa99f
Josué Daniel Marroquín Hernández <jmarroquin-2020296@kinal.edu.gt>
2021-08-30 21:06:15 -0600

    Calculadora Web

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 
-
 const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
